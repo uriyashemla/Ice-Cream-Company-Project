2f509f0730cd62457d861b12de83438c93fcbb21
uriyashemla <uriyashem1994@gmail.com>
2022-10-24 19:55:54 +0300

    fixes

--- Services/StreamLayer/model/kafka.js
+++ Services/StreamLayer/model/kafka.js
@@ -6,7 +6,7 @@ const kafkaConsumer = new KafkaConsumer(require("../../config/kafka.config"));
 kafkaConsumer
     .on("ready", (arg) => {
         kafkaConsumer.subscribe([topic]).consume();
-        console.log(`Consumer ${arg.name} ready. topics: ${topic}`);
+        console.log(`Consumer ${arg.name} ready. topic: ${topic}`);
     })
     .on("disconnected", (arg) =>{
         console.log(`Consumer ${arg.name} disconnected.`)
